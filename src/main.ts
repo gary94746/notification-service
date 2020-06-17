960cbd3b8b0746e8d5c9a95219796c70b2a4619d
gary94746 <gary.109885@gmail.com>
2020-06-17 12:03:43 -0500

    add port

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(PORT, () => {
+    Logger.log(`App running on PORT ${PORT}`, 'NestApplication');
+  });
+}
+bootstrap();
